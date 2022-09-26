28c29ff29d22bd1d1a971b535777fa8285b7845d
karo <karold.castanor@uqvirtual.edu.co>
2022-09-26 12:26:25 -0500

    Primer commit <3

---
+++ negocio/src/main/java/co/edu/uniquindio/unicine/negocioApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.unicine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class negocioApplication {
+
+    public static void main (String[] args){
+
+        SpringApplication.run(negocioApplication.class, args);
+    }
+}
